a2e26edd990c1deb4fecdd8e5709036e427294bf
LamNguyen2021 <9.4ngoclam@gmail.com>
2021-05-26 15:12:40 +0700

    add 3 input into new component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { NewComponent } from './new/new.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NewComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
