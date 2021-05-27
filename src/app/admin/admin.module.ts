f1c0a42e4ebeeb72b6f9fa5562394c8e4d6e52c9
LamNguyen2021 <9.4ngoclam@gmail.com>
2021-05-27 16:09:20 +0700

    validate new component. Display value of form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { NewComponent } from './new/new.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [NewComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    CKEditorModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
--- src/app/admin/new/new.component.ts
+++ src/app/admin/new/new.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
-  styleUrls: ['./new.component.css']
+  styleUrls: ['./new.component.css'],
 })
 export class NewComponent implements OnInit {
+  url_regex = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 
-  constructor() { }
+  public Editor = ClassicEditor;
 
-  ngOnInit(): void {
+  createForm: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    image: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.url_regex),
+    ]),
+    excerpt: new FormControl('', [
+      Validators.required,
+      Validators.minLength(100),
+    ]),
+    content: new FormControl('', [Validators.required])
+  });
+
+  constructor() {}
+
+  handleSubmitArticle(createForm: any) {
+    if (createForm.invalid) {
+      return;
+    }
+    console.log(createForm.value);
+
   }
 
+  ngOnInit(): void {}
 }
